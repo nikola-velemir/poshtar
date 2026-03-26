1c4eddafe5c6dc0c494a0c337acef030c7982ddb
nikola-velemir <vnexa021@gmail.com>
2026-03-26 10:17:19 +0100

    [refactor] rule logic

--- poshtar-spring/src/test/java/org/nikola/velemir/poshtar/spring/adapter/request/deps/infrastructure/AmbiguousRequestHandler2.java
+++ poshtar-spring/src/test/java/org/nikola/velemir/poshtar/spring/adapter/request/deps/infrastructure/AmbiguousRequestHandler2.java
@@ -3,9 +3,6 @@ package org.nikola.velemir.poshtar.spring.adapter.request.deps.infrastructure;
 import org.nikola.velemir.poshtar.core.annotations.Handler;
 import org.nikola.velemir.poshtar.core.request.handler.RequestHandler;
 import org.nikola.velemir.poshtar.core.types.Unit;
-import org.nikola.velemir.poshtar.spring.adapter.notification.deps.injection.InjectionNotificationSecondHandler;
-import org.nikola.velemir.poshtar.spring.adapter.pipeline.deps.transactional.basic.success.TransactionalPipeline;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Handler
