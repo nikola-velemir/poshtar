e5072b5ffa1cd4c3a854e2279ad3a0afdae97ba4
nikola-velemir <vnexa021@gmail.com>
2026-04-07 00:02:42 +0200

    [refactor] ambiguity rule

--- poshtar-opt/src/main/java/org/nikola/velemir/poshtar/opt/rules/ambiguity/AmbiguityRule.java
+++ poshtar-opt/src/main/java/org/nikola/velemir/poshtar/opt/rules/ambiguity/AmbiguityRule.java
@@ -1,21 +1,11 @@
 package org.nikola.velemir.poshtar.opt.rules.ambiguity;
 
-import org.nikola.velemir.poshtar.core.annotations.Handler;
-import org.nikola.velemir.poshtar.core.request.handler.RequestHandler;
 import org.nikola.velemir.poshtar.opt.rules.Rule;
 import org.nikola.velemir.poshtar.opt.rules.RuleContext;
 
 import javax.annotation.processing.RoundEnvironment;
-import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AmbiguityRule implements Rule {
