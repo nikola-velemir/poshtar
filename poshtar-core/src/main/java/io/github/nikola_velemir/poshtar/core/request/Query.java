0d7f06e29be674d8eb067c328ea8bec7035f0e95
nikola-velemir <vnexa021@gmail.com>
2026-09-18 10:32:00 +0200

    feat; extended interfaces to support multiple request types

---
+++ poshtar-core/src/main/java/io/github/nikola_velemir/poshtar/core/request/Query.java
@@ -0,0 +1,7 @@
+package io.github.nikola_velemir.poshtar.core.request;
+
+import io.github.nikola_velemir.poshtar.core.types.Unit;
+
+public interface Query<TResponse> extends Request<TResponse>{
+}
+
---
+++ poshtar-core/src/main/java/io/github/nikola_velemir/poshtar/core/request/VoidCommand.java
@@ -0,0 +1,6 @@
+package io.github.nikola_velemir.poshtar.core.request;
+
+import io.github.nikola_velemir.poshtar.core.types.Unit;
+
+public interface VoidCommand extends Request<Unit>{
+}
