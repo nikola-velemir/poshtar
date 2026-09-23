5230c81c56cb79db0ed6ce10dd5ddd6af738ebf0
Nikola Velemir <148808560+nikola-velemir@users.noreply.github.com>
2026-09-23 11:16:16 +0200

    feat; sealed guice (#43)

--- adapters/poshtar-guice/src/test/java/io/github/nikola_velemir/poshtar/guice/adapter/pipeline/deps/mock/basic/BasicMockRequest.java
+++ adapters/poshtar-guice/src/test/java/io/github/nikola_velemir/poshtar/guice/adapter/pipeline/deps/mock/basic/BasicMockRequest.java
@@ -1,6 +1,7 @@
 package io.github.nikola_velemir.poshtar.guice.adapter.pipeline.deps.mock.basic;
 
+import io.github.nikola_velemir.poshtar.core.request.Query;
 import io.github.nikola_velemir.poshtar.core.request.Request;
 
-public record BasicMockRequest() implements Request<String> {
+public record BasicMockRequest() implements Query<String> {
 }
